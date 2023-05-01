77b67ca178000004ee24663cd61217a61e3410e2
Diego Fernandez <diegofernandezfontana@gmail.com>
2023-05-01 18:59:25 +0200

    Add Task domain class

--- src/domain/Task.ts
+++ src/domain/Task.ts
@@ -1,5 +1,32 @@
-export type Task = {
+type Status = "To do" | "In progress";
+
+export type TaskT = {
   id: string;
   title: string;
   description: string;
+  status: Status;
 };
+
+export class Task implements TaskT {
+  constructor(
+    public id: string,
+    public title: string,
+    public description: string,
+    public status: Status
+  ) {}
+
+  public static MIN_DESCRIPTION_LENGTH = 6;
+
+  public static create(title: string, description: string) {
+    return new Task(this.generateId(), title, description, "To do");
+  }
+
+  private static generateId() {
+    // TODO improve id gen, use VO
+    return String(Math.floor(Math.random() * 100));
+  }
+
+  public static isValid(title: string, description: string) {
+    return title.length > 0 && description.length > Task.MIN_DESCRIPTION_LENGTH;
+  }
+}
